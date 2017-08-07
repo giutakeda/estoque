29b98e3b73aa39b097a7b057ca3c96c1615f2023
Giuliano S. Takeda <giutakeda@gmail.com>
2017-08-07 23:21:46 +0900

    a

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -152,7 +152,7 @@ var app = {
                             var data = field.DataHistorico;
                             var codHistorico = field.CodHistorico;
                             var quantidade = field.Quantidade;
-                            $("#materialSolicitadoList").append('<li onclick="selecionaPendente('+codHistorico+');" class="listaProduto apagar"><div class="seletor" style="width: 30px; heigth: 100%;"><i id="sel'+codHistorico+'" class="invisivel fa fa-square-o fa-2x" aria-hidden="true"></i></div><div class="detalheListaPendente"><span style="font-style: italic; font-weigth: bold;">'+apelido+'</span><br>'+setor+'<br>'+data+'<br>'+quantidade+'<br></div><div class="apagarBtn"><button class="apagar"><i class="fa fa-trash fa-3x" aria-hidden="true"></i></button></div></li>');
+                            $("#materialSolicitadoList").append('<li onclick="selecionaPendente('+codHistorico+');" class="listaProduto apagar"><div class="seletor" style="width: 30px; heigth: 100%;"><i id="sel'+codHistorico+'" class="invisivel fa fa-square-o fa-2x" aria-hidden="true"></i></div><div style="font-size: 22px;" class="detalheListaPendente"><span style="font-style: italic; font-weigth: bold;">'+apelido+'</span><br>'+setor+'<br>'+data+'<br>'+quantidade+'<br></div><div class="apagarBtn"><button class="apagar"><i class="fa fa-trash fa-3x" aria-hidden="true"></i></button></div></li>');
                         });
                         $("#totalProduto").html(json.totalPendente);
                         $("#materialSolicitadoList").on("swipeleft", function(event){
@@ -193,3 +193,9 @@ function selecionaPendente(codHistorico){
         $("#sel"+codHistorico).toggleClass('fa-square-o');
     }
 }      
+
+function goto(page){
+    
+    $('body').pagecontainer('change', '#'+page, {transition: 'flip'});    
+    
+}
